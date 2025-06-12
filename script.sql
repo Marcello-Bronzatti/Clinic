daf0c83e45a01d7a73f364300347c6c77f8ff766
MarcelloBronzatti <mbsimogaki@gmail.com>
2025-06-12 15:40:18 -0300

    chore:Atualização do arquivo script.sql

--- script.sql
+++ script.sql
@@ -31,6 +31,10 @@ CREATE TABLE Users (
     Password NVARCHAR(200) NOT NULL
 );
 
+-- Usuário para testes // admin, senha: admin
+   INSERT INTO Users (Username, Password)
+    VALUES ('admin', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=');
+
 -- Índices
 CREATE INDEX idx_appointments_patient ON Appointments(PatientId, ProfessionalId, ScheduledAt);
 CREATE INDEX idx_appointments_professional ON Appointments(ProfessionalId, ScheduledAt);
